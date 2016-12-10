72414f2f2a831a82b1bc66d6388b151210b50c35
samblackmore <samblackmore07@gmail.com>
2016-12-10 18:49:30 -0500

    adding new post endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '3 words' });
 });
+
+router.get('/add', function(req, res, next) {
+  res.render('newpost', { title: 'add post' });
+});
+
+
 
 router.get('/posts', function(req, res){
   var MongoClient = mongodb.MongoClient;
@@ -35,6 +41,36 @@ router.get('/posts', function(req, res){
       })
     }
   })
+})
+
+router.post('/newpost', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017/threewords';
+
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      console.log('Failed to connect');
+    } else {
+      console.log('New post:', req.body.post);
+
+      var post = {
+        words: req.body.post,
+        score: 0
+      };
+
+      var collection = db.collection('posts');
+
+      collection.insert(post, function(err, result){
+        if (err){
+          console.log(post, 'was not added because', err);
+        } else {
+          console.log(post);
+          res.redirect('add');
+        }
+        db.close();
+      });
+  }});
 })
 
 module.exports = router;
